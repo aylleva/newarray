cc2cede05ecfb9e9f3974645acebbcf96194baff
aylleva <147303116+aylleva@users.noreply.github.com>
2024-10-07 17:59:08 +0400

    Update Program.cs

--- arrayprj1/arraypr1/arraypr1/Program.cs
+++ arrayprj1/arraypr1/arraypr1/Program.cs
@@ -1,7 +1,7 @@
 ﻿int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 11, 63 };
 for(int i = 0; i < nums.Length; i++)
 {
-    for (int j = 2; j < nums.Length; j++)
+    for (int j = 2; j < nums[i]; j++)
     {
         if (nums[i] % j == 0 && nums[i]!=2)
         {
